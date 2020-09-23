30bbfd531423cef81844ba68263de36fd51171fa
TiliaSNogueira <tiliasalomao@gmail.com>
2020-09-23 15:38:35 -0300

    Criação das classes Livro e  ColecaoLivro

---
+++ src/br/com/digitalhouse/desafioIntegrador/ColecaoLivro.kt
@@ -0,0 +1,4 @@
+package br.com.digitalhouse.desafioIntegrador
+
+class ColecaoLivro(val descricao: String, codigo: Long, titulo: String, autor: String, anoLancamento: Int, preco: Double) : Livro (codigo, titulo, autor, anoLancamento, preco) {
+}
