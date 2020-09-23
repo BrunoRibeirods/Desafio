bf75b2839071726205e4c948d99b2b46fab26acf
Bruno <brunorbds50@gmail.com>
2020-09-23 15:58:53 -0300

    Estrutura Estoque

---
+++ src/br/com/digitalhouse/desafioIntegrador/Estoque.kt
@@ -0,0 +1,23 @@
+package br.com.digitalhouse.desafioIntegrador
+
+class Estoque(
+        val estoqueLivro: MutableList<Livro> = mutableListOf<Livro>(),
+        val estoqueColecaoLivro: MutableMap<Long, ColecaoLivro> = mutableMapOf<Long, ColecaoLivro>()
+) {
+
+    fun cadastrarLivro(){
+
+    }
+
+    fun cadastrarColecao(){
+
+    }
+
+    fun consultarLivro(){
+
+    }
+
+    fun efetuarVenda(){
+
+    }
+}
